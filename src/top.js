ca4b2ebaa6cd4dfeca0011624a4e32b94a25447f
Sarasa Morita <mr.klee.chi@gmail.com>
2022-09-20 23:25:59 +0900

    /about

--- src/top.js
+++ src/top.js
@@ -1,14 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {React, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Container from './compornent/container';
 import Page2 from './compornent/pages/page2';
 
-const Top = () => (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Container />} />
-        <Route path="about" element={<Page2 />} />
-      </Routes>
-    </Router>
-  );
+function Top()  {
+//   const { pathname } = useLocation();
+
+
+//   useEffect (
+//     () => {
+//         window.scrollTo(0, 0);
+//     },
+//     [pathname]
+// );
+
+  return (<Router>
+    <Routes>
+      <Route path="/" element={<Container />} />
+      <Route path="/about" element={<Page2 />} />
+    </Routes>
+  </Router>
+);
+  }
+ 
+  export default  Top
+
+  // function Top() {
+  //   return (
+  //     <Router>
+  //       <Routes>
+  //         <Route path="/" element={<Container />} />
+  //         <Route path="about" element={<Page2 />} />
+  //       </Routes>
+  //     </Router>
+  //   );
+  // }
   
-  export default Top;
+  // export default Top;
