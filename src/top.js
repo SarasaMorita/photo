4af82c8737d57d078d4529667b159a129c0e9586
Sarasa Morita <mr.klee.chi@gmail.com>
2022-09-22 22:46:38 +0900

    Not found

--- src/top.js
+++ src/top.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes,} from 'react-router-dom';
 import FadeInCon from './compornent/fadeincon';
 import Page2Fade from './compornent/page2Fade';
+import NotFound from './compornent/notfound';
 
 function Top()  {
 
@@ -11,6 +12,7 @@ function Top()  {
     <Routes>
       <Route path="/" element={<FadeInCon />} />
       <Route path="/about" element={<Page2Fade/>} />
+      <Route path="/*" element={<NotFound/>} />
     </Routes>
   </Router>
 );
