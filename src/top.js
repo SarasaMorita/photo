7d7d94ee2be09dd931cb71f193d638a8d92edb1c
Sarasa Morita <mr.klee.chi@gmail.com>
2022-09-30 21:05:12 +0900

    もとに戻してもエラー

--- src/top.js
+++ src/top.js
@@ -4,7 +4,7 @@ import { HashRouter as Router, Route, Routes,} from 'react-router-dom';
 import Loading from './Loading/Loading';
 import Page2Now from './compornent/pages/Page2Now';
 import NotFound from './compornent/notfound';
-import Photoanime from './photoanime';
+import Page3Loading from './compornent/portforio/Page3Loading';
 
 function Top()  {
 
@@ -13,7 +13,7 @@ function Top()  {
     <Routes>
       <Route path="/" element={<Loading />} />
       <Route path="/about" element={<Page2Now/>} />
-      <Route path="/machi" element={<Photoanime/>} />
+      <Route path="/machi" element={<Page3Loading/>} />
       <Route path="/*" element={<NotFound/>} />
     </Routes>
   </Router>
